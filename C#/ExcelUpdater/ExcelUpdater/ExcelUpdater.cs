e7071e352bb815c0733498645b716f483bd6fe9d
Carlos Raul Garcia <crgar@microsoft.com>
2019-07-22 23:10:24 +0200

    little refactor

--- C#/ExcelUpdater/ExcelUpdater/ExcelUpdater.cs
+++ C#/ExcelUpdater/ExcelUpdater/ExcelUpdater.cs
@@ -7,33 +7,72 @@
 
     class ExcelUpdater
     {
-        public void UpdateExcel(string filePath, int initialRow, int copyUntilMaxPlusRows)
+        private string CalculateColumnLettersFromIndex(int columnIndex)
+        {
+            // Calculate the column letter excel style: 1 => A, 2 => B
+            string columnLetter = "";
+            int tempIndex = columnIndex;
+            while (tempIndex > 26)
+            {
+                tempIndex -= 26;
+                columnLetter = string.IsNullOrEmpty(columnLetter) ? "A" : ((char)(columnLetter[0] + 1)).ToString();
+            }
+
+            columnLetter += ((char)((columnIndex - 1) % 26 + 65)).ToString();
+
+            return columnLetter;
+        }
+
+        private int FindMaxItemIndexInRange(Range copyFrom)
+        {
+            float maxValue = -1;
+            int maxValueIndex = 1;
+            int index = 1;
+            // Copy data column
+
+            maxValueIndex = FindMaxItemIndexInRange(copyFrom);
+            foreach (Range cell in copyFrom)
+            {
+                float cellValue = float.Parse(cell.Value2.ToString());
+                if (cellValue > maxValue)
+                {
+                    maxValue = cellValue;
+                    maxValueIndex = index;
+                }
+                index++;
+            }
+
+            return maxValueIndex;
+        }
+
+        public void UpdateExcel(string filePath, int initialRowIndex, int copyUntilMaxPlusRows)
         {
             Application oXL;
             _Workbook oWB;
             _Worksheet inputSheet, outputSheet;
 
-            //Start Excel and get Application object.
+            // Start Excel and get Application object.
             oXL = new Application();
             oXL.Visible = true;
 
-            //Get a new workbook.
+            // Get a new workbook.
             oWB = (_Workbook)(oXL.Workbooks.Open(filePath));
             inputSheet = (_Worksheet)oWB.Sheets.Item["input"];
             outputSheet = (_Worksheet)oWB.Sheets.Item["output"];
 
-            int maxRow = initialRow;
-            while(!string.IsNullOrWhiteSpace(inputSheet.Cells[maxRow+1, 1].Text))
+            // maxRowIndex = index of the first blank cell in the first column
+            int maxRowIndex = initialRowIndex;
+            while(!string.IsNullOrWhiteSpace(inputSheet.Cells[maxRowIndex+1, 1].Text))
             {
-                maxRow++;
+                maxRowIndex++;
             }
 
-            int initialDataRow = initialRow + 1;
-            int dataRowCount = maxRow - initialDataRow + 1;
-           
-            int columnIndex = 2;
-            int nextOutRowStart = 1;
-            string columnName = inputSheet.Cells[initialRow, columnIndex].Text;
+            // INITIALIZATION DATA
+            int initialDataRow = initialRowIndex + 1;               // First are column names titles
+            int dataRowCount = maxRowIndex - initialDataRow + 1;    // How many rows of pure data do we have per column
+            int columnIndex = 2;                                    // First column is datetime
+            int nextOutRowStart = 1;                                // Which row we start writing in the Output sheet
+            string columnName = inputSheet.Cells[initialRowIndex, columnIndex].Text;
 
             while (!string.IsNullOrWhiteSpace(columnName))
             {
@@ -42,55 +81,26 @@
                 int dataRowCountToCopy = dataRowCount;
 
                 // Calculate the column letter excel style: 1 => A, 2 => B
-                string columnLetter = "";
-                int tempIndex = columnIndex;
-                while(tempIndex > 26)
-                {
-                    tempIndex -= 26;
-                    columnLetter = string.IsNullOrEmpty(columnLetter) ? "A" : ((char)(columnLetter[0] + 1)).ToString();
-                }
-
-                columnLetter += ((char)((columnIndex - 1) % 26 + 65)).ToString(); 
-
-                if(columnIndex % 25 == 0)
-                {
-                    Debugger.Break();
-                }
+                string columnLetter = CalculateColumnLettersFromIndex(columnIndex);
 
                 // We need to find the max value and copy until maxvalue + copyUntilMaxPlusRows
                 if (copyUntilMaxPlusRows >= 0)
                 {
-                    float maxValue = -1;
-                    int maxValueIndex = 1;
-                    int index = 1;
-                    // Copy data column
-                    copyFrom = inputSheet.Range[columnLetter + initialDataRow.ToString(), columnLetter + maxRow.ToString()];
+                     // Copy data column
+                    copyFrom = inputSheet.Range[columnLetter + initialDataRow.ToString(), columnLetter + maxRowIndex.ToString()];
 
-                    foreach (Range cell in copyFrom)
-                    {
-                        float cellValue = float.Parse(cell.Value2.ToString());
-                        if(cellValue > maxValue)
-                        {
-                            maxValue = cellValue;
-                            maxValueIndex = index;
-                        }
-                        index++;
-                    }
+                    int maxValueIndex = FindMaxItemIndexInRange(copyFrom);
 
                     // we will copy only (maxValueIndex + copyUntilMaxPlusRows), or dataRowCount if we get out of range
                     dataRowCountToCopy = maxValueIndex + copyUntilMaxPlusRows;
                     dataRowCountToCopy = dataRowCountToCopy < dataRowCount ? dataRowCountToCopy : dataRowCount;
                 }
 
+                // Important row index that we will use to copy all columns
                 var sourceDataInitialIndex      = initialDataRow.ToString();
                 var sourceDataEndIndex          = (initialDataRow + dataRowCountToCopy - 1).ToString();
                 var destinationDataInitialIndex = nextOutRowStart.ToString();
                 var destinationDataEndIndex     = (nextOutRowStart + dataRowCountToCopy -1).ToString();
-
-                // Copy data column
-                copyFrom = inputSheet.Range[columnLetter + sourceDataInitialIndex, columnLetter + sourceDataEndIndex];
-                copyTo = outputSheet.Range['C' + destinationDataInitialIndex, 'C' + destinationDataEndIndex];
-                copyTo.Value2 = copyFrom.Value2;
 
                 // Copy columnName column
                 string[] columnNameColumn = new string[dataRowCountToCopy];
@@ -103,11 +113,16 @@
                 copyTo = outputSheet.Range['B' + destinationDataInitialIndex, 'B' + destinationDataEndIndex];
                 copyTo.Value2 = copyFrom.Value2;
 
+                // Copy data column
+                copyFrom = inputSheet.Range[columnLetter + sourceDataInitialIndex, columnLetter + sourceDataEndIndex];
+                copyTo = outputSheet.Range['C' + destinationDataInitialIndex, 'C' + destinationDataEndIndex];
+                copyTo.Value2 = copyFrom.Value2;
+
                 // Next iteration indexes
                 columnIndex++;
                 columnLetter = "";               
                 nextOutRowStart += dataRowCountToCopy;
-                columnName = inputSheet.Cells[initialRow, columnIndex].Text;
+                columnName = inputSheet.Cells[initialRowIndex, columnIndex].Text;
             }
         }
     }
